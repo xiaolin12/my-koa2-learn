10f7336b3c09ef87ed1f1c2fcd545d5142127cbd
jianlin mei <1291247841@qq.com>
2020-03-10 13:30:43 +0800

    First commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  global.console.log('index')
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+router.get('/testAsync',async(ctx)=>{
+  global.console.log('start',new DataCue())
+})
+
+module.exports = router
