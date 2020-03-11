d695941de7ce075cbcc71c49b777f4b5d7e1dc52
xiaolin12 <1291247841@qq.com>
2020-03-11 21:50:41 +0800

    learn the middleware

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,19 @@ router.get('/json', async (ctx, next) => {
   }
 })
 
+// 有了async，没有await，会执行异步操作，有了await，会顺序执行
 router.get('/testAsync',async(ctx)=>{
-  global.console.log('start',new DataCue())
+  global.console.log('start',new Date().getTime())
+  const a = await new Promise((resolve,reject)=>{
+    setTimeout(function () {
+      global.console.log('async a',new Date().getTime())
+      resolve('return')
+    },1000)
+  })
+  ctx.body={
+    a
+  }
 })
+
 
 module.exports = router
